2c5192b910df435eb9eb83e30e230f9b15f0d159
Icygoose <hbc0826@gmail.com>
2025-05-08 02:40:24 +0800

    update settings with flask port

--- routes/settings.py
+++ routes/settings.py
@@ -8,7 +8,7 @@ settings = Blueprint('settings', __name__)
 @settings.route('/settings')
 @login_required
 def view_settings():
-    return render_template('Settings/pages/settings.html')
+    return render_template('Settings/settings.html', user=current_user)
 
 @settings.route('/settings/update-password', methods=['POST'])
 @login_required
