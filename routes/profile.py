86d38037cae7586a87473e18df0613e60510a948
Icygoose <hbc0826@gmail.com>
2025-05-08 15:24:07 +0800

    Uptate updating-profile feature

--- routes/profile.py
+++ routes/profile.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, current_app
 from flask_login import login_required, current_user
 from models import db, User, Achievement
 from datetime import datetime
+import os
+from werkzeug.utils import secure_filename
 
 profile = Blueprint('profile', __name__)
 
@@ -27,8 +29,16 @@ def edit_profile():
         
         # Handle profile image upload (if provided)
         if 'editProfilePic' in request.files and request.files['editProfilePic'].filename:
-            # Code to save image file would go here
-            pass
+            file = request.files['editProfilePic']
+            # Secure the filename
+            filename = secure_filename(file.filename)
+            # Create a unique filename to avoid overwrites
+            unique_filename = f"{current_user.username}_{datetime.now().strftime('%Y%m%d%H%M%S')}_{filename}"
+            # Save the file
+            file_path = os.path.join(current_app.static_folder, 'images', unique_filename)
+            file.save(file_path)
+            # Update the user's profile picture field (store relative path)
+            current_user.profile_pic = f'images/{unique_filename}'
         
         db.session.commit()
         
