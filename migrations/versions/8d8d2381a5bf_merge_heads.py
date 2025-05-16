61531d1086ba0cd006704b4030903543b476b4ad
closer512 <guoxuting2301@163.com>
2025-05-16 20:42:12 +0800

    Solve the database migration problem caused by file merging

---
+++ migrations/versions/8d8d2381a5bf_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 8d8d2381a5bf
+Revises: c3d4e5f6a7b8, c8623d5c0230
+Create Date: 2025-05-16 20:38:37.359669
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8d8d2381a5bf'
+down_revision = ('c3d4e5f6a7b8', 'c8623d5c0230')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
